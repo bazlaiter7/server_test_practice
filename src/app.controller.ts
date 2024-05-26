1a7195e52277ffdfacb1270da43dded6d63ab229
Nick <java.2015@bk.ru>
2024-05-26 22:40:32 +0600

    added server side payload checking

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,9 +7,18 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
   @Get('/api/v1/request')
   getHello(@Req() request: Request) {
+    
+    if(request.body.wait < 0){
+      console.log("error, wait too few");
+      return "error, wait too few";
+    }
+    if(request.body.answer.length < 3){
+      console.log("error, answer string is too few");
+      return "error, answer string is too few";
+    }
     setTimeout(()=>{
       console.log(request.body.answer);
       return request.body.answer;
-    }, request.body.n)
+    }, request.body.wait)
   }
 }
